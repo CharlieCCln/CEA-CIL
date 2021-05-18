505bd9cf816bf90cb4788180aa3cc58c070adfd2
Charlie CCln <43385667+CharlieCCln@users.noreply.github.com>
2021-05-18 11:45:06 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@
   function draw() {
     background(150);
     // Draw the video
-    image(video, 0, 0, width, width * video.height / video.width);
+    image(flippedVideo, 0, 0, width, width * video.height / video.width);
 
     // Draw the label
     fill(255);
