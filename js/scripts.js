66abba5f4dbf661f976d11fb216a6f8f48784056
Charlie CCln <43385667+CharlieCCln@users.noreply.github.com>
2021-05-27 15:00:11 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Classifier Variable
   let classifier;
   // Model URL
-  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/SPfvBqa_U/';
+  let imageModelURL = 'https://teachablemachine.withgoogle.com/models/lIAiQGGPl/';
 
   // Video
   let capture;
